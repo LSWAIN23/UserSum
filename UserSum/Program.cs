dbaf15febbe66fbb7040edec5511a46cb27af0c7
hackercentral <levi.swain1@outlook.com>
2018-11-06 19:26:34 -0500

    initial commit

---
+++ UserSum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Make a console application that takes 2 numbers as user input, and returns the sum
+
+namespace UserSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter a number..");
+            double First = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter another number..");
+            double Second = double.Parse(Console.ReadLine());
+            Console.WriteLine(AddValues(First, Second));
+        }
+
+        static public double AddValues(double First, double Second)
+        {
+            double sum = First + Second;
+            Console.WriteLine($"The sum of these two number is: ");
+            return sum;
+        }
+    }
+}
